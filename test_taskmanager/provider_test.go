507fd5f0107427db5d5d7a3070bc339b05bcab21
shivraj1192 <2020bpr012@sggs.ac.in>
2025-06-11 13:36:03 +0530

    Main - Updated tests

--- test_taskmanager/provider_test.go
+++ test_taskmanager/provider_test.go
@@ -1,28 +1,50 @@
-package test
+package testtaskmanager_test
 
 import (
+	"errors"
+	"fmt"
+	"os"
+	"terraform-provider-taskmanager/taskmanager"
 	"testing"
 
-	"terraform-provider-taskmanager/taskmanager"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/mitchellh/go-homedir"
 )
 
-// providerFactories is a map of the provider factories used for testing
-var providerFactories = map[string]func() (*schema.Provider, error){
-	"taskmanager": func() (*schema.Provider, error) {
-		return taskmanager.Provider("test"), nil
-	},
+var testAccProviders map[string]*schema.Provider
+var testAccProvider *schema.Provider
+var testVersion = "1.0.0"
+
+func init() {
+	testAccProvider = taskmanager.Provider(testVersion)
+	testAccProviders = map[string]*schema.Provider{
+		"taskmanager": testAccProvider,
+	}
 }
 
-// TestProvider tests that the provider can be instantiated
 func TestProvider(t *testing.T) {
-	if err := taskmanager.Provider("test").InternalValidate(); err != nil {
+	if err := taskmanager.Provider(testVersion).InternalValidate(); err != nil {
 		t.Fatalf("err: %s", err)
 	}
 }
 
-// TestProviderImpl tests that the provider implementation satisfies the interface
-func TestProviderImpl(t *testing.T) {
-	var _ *schema.Provider = taskmanager.Provider("test")
+func TestProvider_impl(t *testing.T) {
+	var _ *schema.Provider = taskmanager.Provider(testVersion)
+}
+
+func testAccPreCheck(t *testing.T) {
+	configPath, _ := homedir.Expand("~/.taskmanager/tf.config")
+	if _, err := os.Stat(configPath); !os.IsNotExist(err) {
+		return
+	}
+	if err := os.Getenv("BASE_URL"); err == "" {
+		t.Fatal("BASE_URL must be set for acceptance tests")
+	}
+	if err := os.Getenv("TOKEN"); err == "" {
+		t.Fatal("TOKEN must be set for acceptance tests")
+	}
+}
+
+func NewNotFoundErrorf(format string, a ...interface{}) error {
+	return fmt.Errorf("%w %s", errors.New("Could not found"), fmt.Sprintf(format, a...))
 }
